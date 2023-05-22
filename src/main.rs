21ae264ef79e7c31c3107b481598cdd50c75225d
Epsylene <epsylene@gmail.com>
2023-05-22 16:36:13 +0200

    Minimal window working

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,76 @@
-use image::{RgbImage, ImageBuffer, Rgb};
+use image::{RgbImage, Rgb};
+use pixels::{SurfaceTexture, Pixels};
+use winit::{window::Window, event_loop::{EventLoop, ControlFlow}, dpi::LogicalSize, window::WindowBuilder, event::{Event, VirtualKeyCode}};
+use winit_input_helper::WinitInputHelper;
 
-fn main() {
-    const IMG_WIDTH: u32 = 256;
-    const IMG_HEIGHT: u32 = 256;
+const WIDTH: u32 = 320;
+const HEIGHT: u32 = 240;
 
-    let mut buffer: RgbImage = ImageBuffer::new(IMG_WIDTH, IMG_HEIGHT);
+fn draw(frame: &mut [u8]) {
+    for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
+        let x = i % WIDTH as usize;
+        let y = i / WIDTH as usize;
 
-    for (x, y, pixel) in buffer.enumerate_pixels_mut() {
-        let r = x as f64 / (IMG_WIDTH-1) as f64;
-        let g = y as f64 / (IMG_HEIGHT-1) as f64;
+        let r = x as f64 / (WIDTH-1) as f64;
+        let g = y as f64 / (HEIGHT-1) as f64;
         let b = 0.25;
 
         let ir = (255.999 * r) as u8;
         let ig = (255.999 * g) as u8;
         let ib = (255.999 * b) as u8;
 
-        *pixel = Rgb([ir, ig, ib]);
+        pixel.copy_from_slice(&[ir, ig, ib, 255])
     }
+}
 
-    match buffer.save("image.png") 
-    {
-        Err(e) => eprintln!("Error writing file: {e}"),
-        Ok(()) => println!("Done.")
+fn main() {
+    let event_loop = EventLoop::new();
+    let mut input = WinitInputHelper::new();
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        WindowBuilder::new()
+            .with_title("Hello Pixels")
+            .with_inner_size(size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
     };
+
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(WIDTH, HEIGHT, surface_texture).unwrap()
+    };
+
+    event_loop.run(move |event, _, control_flow| {
+        // Draw the current frame
+        if let Event::RedrawRequested(_) = event {
+            draw(pixels.frame_mut());
+            if let Err(err) = pixels.render() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+        }
+
+        // Handle input events
+        if input.update(&event) {
+            // Close events
+            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+                *control_flow = ControlFlow::Exit;
+                return;
+            }
+
+            // Resize the window
+            if let Some(size) = input.window_resized() {
+                if let Err(err) = pixels.resize_surface(size.width, size.height) {
+                    // log_error("pixels.resize_surface", err);
+                    *control_flow = ControlFlow::Exit;
+                    return;
+                }
+            }
+
+            // Update internal state and request a redraw
+            window.request_redraw();
+        }
+    });
 }
