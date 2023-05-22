33527921fbaf134d6da278f2ba8bbbc398f469a6
Epsylene <epsylene@gmail.com>
2023-05-22 23:20:58 +0200

    Minimal egui working

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
-use image::{RgbImage, Rgb};
+use crate::gui::Framework;
+
 use pixels::{SurfaceTexture, Pixels};
-use winit::{window::Window, event_loop::{EventLoop, ControlFlow}, dpi::LogicalSize, window::WindowBuilder, event::{Event, VirtualKeyCode}};
+use winit::{event_loop::{EventLoop, ControlFlow}, dpi::LogicalSize, window::WindowBuilder, event::{Event, VirtualKeyCode}};
 use winit_input_helper::WinitInputHelper;
 
-const WIDTH: u32 = 320;
-const HEIGHT: u32 = 240;
+mod gui;
+
+const WIDTH: u32 = 720;
+const HEIGHT: u32 = 576;
 
 fn draw(frame: &mut [u8]) {
     for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
@@ -36,41 +39,81 @@ fn main() {
             .unwrap()
     };
 
-    let mut pixels = {
+    let (mut pixels, mut framework) = {
         let window_size = window.inner_size();
+        let scale_factor = window.scale_factor() as f32;
         let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-        Pixels::new(WIDTH, HEIGHT, surface_texture).unwrap()
+        let pixels = Pixels::new(WIDTH, HEIGHT, surface_texture).unwrap();
+        let framework = Framework::new(
+            &event_loop,
+            window_size.width,
+            window_size.height,
+            scale_factor,
+            &pixels,
+        );
+
+        (pixels, framework)
     };
 
     event_loop.run(move |event, _, control_flow| {
-        // Draw the current frame
-        if let Event::RedrawRequested(_) = event {
-            draw(pixels.frame_mut());
-            if let Err(err) = pixels.render() {
-                *control_flow = ControlFlow::Exit;
-                return;
-            }
-        }
-
         // Handle input events
         if input.update(&event) {
             // Close events
-            if input.key_pressed(VirtualKeyCode::Escape) || input.quit() {
+            if input.key_pressed(VirtualKeyCode::Escape) || input.close_requested() || input.destroyed() {
                 *control_flow = ControlFlow::Exit;
                 return;
             }
 
             // Resize the window
+            if let Some(scale) = input.scale_factor() {
+                framework.scale_factor(scale);
+            }
+
+            // Resize the window
             if let Some(size) = input.window_resized() {
                 if let Err(err) = pixels.resize_surface(size.width, size.height) {
-                    // log_error("pixels.resize_surface", err);
+                    println!("pixels.resize_surface error, {:?}", err);
                     *control_flow = ControlFlow::Exit;
                     return;
                 }
+                framework.resize(size.width, size.height);
             }
 
             // Update internal state and request a redraw
             window.request_redraw();
+        }
+
+        match event {
+            Event::WindowEvent { event, .. } => {
+                // Update egui inputs
+                framework.handle_event(&event);
+            }
+            // Draw the current frame
+            Event::RedrawRequested(_) => {
+                // Draw the world
+                draw(pixels.frame_mut());
+
+                // Prepare egui
+                framework.prepare(&window);
+
+                // Render everything together
+                let render_result = pixels.render_with(|encoder, render_target, context| {
+                    // Render the world texture
+                    context.scaling_renderer.render(encoder, render_target);
+
+                    // Render egui
+                    framework.render(encoder, render_target, context);
+
+                    Ok(())
+                });
+
+                // Basic error handling
+                if let Err(err) = render_result {
+                    println!("pixels.render error, {:?}", err);
+                    *control_flow = ControlFlow::Exit;
+                }
+            }
+            _ => (),
         }
     });
 }
